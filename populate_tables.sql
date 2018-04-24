11e930a230bc4e608d414000483e17fa671bea64
Joshua Stephen Vega <jsvcycling.live@gmail.com>
2018-04-24 13:36:05 -0400

    More sample data work.

--- populate_tables.sql
+++ populate_tables.sql
@@ -1,8 +1,8 @@
 -- CUSTOMER sample data.
 INSERT INTO customer VALUES
-  (1, 'Juana', 'Smith', 'juana.smith@example.com', '862-555-1234', 'R'),
-  (2, 'Joseph', 'Cowen', 'joseph.cowen@example.com', '973-555-5678', 'R'),
-  (3, 'Anita', 'Boudreaux', 'anita.bourdreaux@example.com', '201-555-9012', 'S');
+  (1, 'Juana', 'Smith', 'juana.smith@example.com', '862-555-1234', 'Regular'),
+  (2, 'Joseph', 'Cowen', 'joseph.cowen@example.com', '973-555-5678', 'Regular'),
+  (3, 'Anita', 'Boudreaux', 'anita.bourdreaux@example.com', '201-555-9012', 'Silver');
 
 -- SILVER_AND_ABOVE sample data.
 INSERT INTO customer VALUES
@@ -15,8 +15,33 @@ INSERT INTO shipping_address VALUES
 
 -- CREDIT_CARD sample data.
 INSERT INTO credit_card VALUES
+  (234567890123, 234, 'Joseph Cowen', 'MasterCard', 1587769200),
   (123456789012, 123, 'Juana Smith', 'Visa', 1587686400);
 
 -- STORED_CARD sample data.
 INSERT INTO stored_card VALUES
   (123456789012, 1);
+
+-- CART sample data.
+INSERT INTO cart VALUES
+  (1, 1, 'Home', 123456789012, 'Open', NULL);
+
+-- PRODUCT sample data.
+INSERT INTO product VALUES
+  (1, 'Laptop', 'Dell XPS 13', 1299.99, 'A lightweight laptop from Dell.', 10);
+
+-- APPEARS_IN sample data.
+INSERT INTO appears_in VALUES
+  (1, 1, 1, 1299.99);
+
+-- OFFER_PRODUCT sample data.
+
+-- COMPUTER sample data.
+INSERT INTO computer VALUES
+  (1, 'Intel i7');
+
+-- LAPTOP sample data.
+INSERT INTO laptop VALUES
+  (1, '74 Wh', 2.1);
+
+-- PRINTER sample data.
