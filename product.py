3b86006268d5c46be49d90abc6b8ada3ad324400
Tarvos99 <jasgrant90@gmail.com>
2018-04-29 09:53:04 -0400

    Update product.py

--- product.py
+++ product.py
@@ -10,8 +10,69 @@ product_bp = Blueprint('product_bp', __name__)
 #-----------------------------
 @product_bp.route('/', methods=['GET'])
 def list_products():
-    pass
+    # return list of all products. if user wants more info on product, search specific product id in next method
+    if request.method == 'GET':
+        return render_template('product/product_search.html')
+    else:
+        c = db.cursor()
+        c.execute("""
+            SELECT * FROM product
+            """, )
+        items = c.fetchall()
+        c.close()
+
+        return render_template('product/product_results.html', prodc=items)
+
+
+
 
 @product_bp.route('/<int:product_id>', methods=['GET'])
 def show_product(product_id):
-    pass
+    if request.method == 'GET':
+        return render_template('product/product_search.html')
+    else:
+        c = db.cursor()
+        c.execute("""
+            SELECT * FROM product
+            WHERE PID=? """, (
+            product_id)
+            , )
+        item = c.fetchone()
+
+        # check if item is a laptop
+        c = db.cursor()
+        c.execute("""
+            SELECT * FROM laptop
+            WHERE PID=? """, (
+            product_id)
+            , )
+        itemlp = c.fetchone()
+
+        # check if item is a printer
+        c = db.cursor()
+        c.execute("""
+                  SELECT * FROM printer
+                  WHERE PID=? """, (
+            product_id)
+                  , )
+        itempr = c.fetchone()
+
+        # check if item is a computer
+        c = db.cursor()
+        c.execute("""
+                  SELECT * FROM computer
+                  WHERE PID=? """, (
+            product_id)
+                  , )
+
+        itemcmp = c.fetchone()
+        c.close()
+        # depending on type of product, return different template
+        if itemlp:
+            return render_template('product/product_results.html', prodccl=itemlp, prodc=item)
+        elif itemcmp:
+            render_template('product/product_results.html', prodcc=itemcmp, prodc=item)
+        elif itempr:
+            render_template('product/product_results.html', prodcp=itempr, prodc=item)
+        else:
+            return render_template('product/product_results.html', prodc=item)
