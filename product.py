5276bdae5a1955f4ae905196a3f3692ef82a2cf7
Joshua Stephen Vega <jsvcycling.live@gmail.com>
2018-04-30 14:38:16 -0400

    Wrote a search page based on Jason's work.

--- product.py
+++ product.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, session, g
+from flask import Blueprint, request, session, g, render_template
+
+from db import db
 
 # The product-based routes.
 product_bp = Blueprint('product_bp', __name__)
@@ -10,21 +12,29 @@ product_bp = Blueprint('product_bp', __name__)
 #-----------------------------
 @product_bp.route('/', methods=['GET'])
 def list_products():
-    # return list of all products. if user wants more info on product, search specific product id in next method
-    if request.method == 'GET':
-        return render_template('product/product_search.html')
+    q = request.args.get('query', '', type=str)
+    prods = None
+    user = None
+    
+    c = db.cursor()
+
+    if len(q) > 0:
+        # Bad idea. Doesn't protect against SQL injection.
+        c.execute('SELECT * FROM product WHERE PName LIKE \'%{}%\''.format(q))
+        prods = c.fetchall()
     else:
-        c = db.cursor()
-        c.execute("""
-            SELECT * FROM product
-            """, )
-        items = c.fetchall()
-        c.close()
+        c.execute('SELECT * FROM product')
+        prods = c.fetchall()
 
-        return render_template('product/product_results.html', prodc=items)
+    if 'cid' in session:
+        c.execute('SELECT * FROM customer WHERE CID=?', (session['cid'],))
+        user = c.fetchone()
 
+    # TODO: If the user exists and they are Gold or Platinum status, show the
+    #       discounted price (e.g. cross out original price).
 
-
+    c.close()
+    return render_template('product/list_products.html', prods=prods, user=user, q=q)
 
 @product_bp.route('/<int:product_id>', methods=['GET'])
 def show_product(product_id):
