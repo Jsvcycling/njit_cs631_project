b6d42eca182842cacaa8746c84f66ec7e98748e2
Josh Vega <jsvcycling.live@gmail.com>
2018-05-01 18:28:27 -0400

    Added best customers statistic.

--- statistics.py
+++ statistics.py
@@ -93,15 +93,15 @@ def most_unique_sold():
         c.execute('SELECT * from customer WHERE CID=?', (session['cid'],))
         user = c.fetchone()
 
+    c.close()
+
     for idx, prod in enumerate(prods):
         prod = dict(zip(prod.keys(), prod))
         prods[idx] = prod
 
-    c.close()
+    prods.sort(key=operator.itemgetter('NumCust'), reverse=True)
 
     print(prods)
-
-    prods.sort(key=operator.itemgetter('NumCust'), reverse=True)
 
     return render_template('statistics/most_unique.html', user=user,
                            prods=prods, start_time=start_time_str,
@@ -124,6 +124,17 @@ def best_customers():
                                      '%d/%m/%Y %H:%M:%S %p').timestamp()
 
     c = db.cursor()
+    c.execute("""
+    SELECT c.CID AS CID, c.FName AS FName, c.LName AS LName,
+    SUM(a.TotalPrice) AS TotalPrice
+    FROM customer c
+    JOIN cart b ON c.CID = b.CID
+    JOIN (SELECT CartID, SUM(Quantity * PriceSold) AS TotalPrice
+    FROM appears_in GROUP BY CartID) a ON b.CartID = a.CartID
+    WHERE b.TDate > ? AND b.TDate < ? AND b.TStatus = ?
+    GROUP BY c.CID, c.FName, c.LName LIMIT 10
+    """, (start_time, end_time, 'Purchased',))
+    custs = c.fetchall()
 
     user = None
 
@@ -133,8 +144,17 @@ def best_customers():
 
     c.close()
 
+    for idx, cust in enumerate(custs):
+        cust = dict(zip(cust.keys(), cust))
+        custs[idx] = cust
+
+    custs.sort(key=operator.itemgetter('TotalPrice'), reverse=True)
+
+    print(custs)
+
     return render_template('statistics/best_customers.html', user=user,
-                           start_time=start_time_str, end_time=end_time_str)
+                           custs=custs, start_time=start_time_str,
+                           end_time=end_time_str)
 
 @statistics_bp.route('/best_zipcodes', methods=['GET'])
 def best_zipcodes():
@@ -176,6 +196,31 @@ def best_zipcodes():
     return render_template('statistics/best_zipcodes.html', user=user, zips=zips,
                            start_time=start_time_str, end_time=end_time_str)
 
-@statistics_bp.route('/avg_price')
+@statistics_bp.route('/avg_price', methods=['GET'])
 def avg_price():
-    pass
+    start_time = 0
+    end_time = datetime.now().timestamp()
+
+    start_time_str = request.args.get('start_time', '', type=str)
+    end_time_str   = request.args.get('end_time', '', type=str)
+
+    if len(start_time_str) > 0:
+        start_time = datetime.strptime(start_time_str,
+                                       '%d/%m/%Y %H:%M:%S %p').timestamp()
+
+    if len(end_time_str) > 0:
+        end_time = datetime.strptime(end_time_str,
+                                     '%d/%m/%Y %H:%M:%S %p').timestamp()
+
+    c = db.cursor()
+
+    user = None
+
+    if 'cid' in session:
+        c.execute('SELECT * FROM customer WHERE CID=?', (session['cid'],))
+        user = c.fetchone()
+
+    c.close()
+
+    return render_template('statistics/avg_price.html', user=user,
+                           start_time=start_time_str, end_time=end_time_str)
